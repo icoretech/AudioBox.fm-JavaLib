6b1bd426868b84a2e9f39eb6f7c0f39f490618c9
fatshotty <fat@fatshotty.net>
2010-06-17 22:26:24 +0200

    upload progress

--- AudioBoxSync/src/fm/audiobox/sync/Upload.java
+++ AudioBoxSync/src/fm/audiobox/sync/Upload.java
@@ -7,16 +7,23 @@ import org.apache.http.client.methods.HttpPost;
 import fm.audiobox.api.AudioBoxClient;
 import fm.audiobox.api.exceptions.LoginException;
 import fm.audiobox.api.exceptions.ServiceException;
+import fm.audiobox.api.interfaces.ThreadListener;
 import fm.audiobox.api.models.Track;
 import fm.audiobox.util.ThreadItem;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.apache.http.entity.mime.content.FileBody;
 
 public class Upload extends ThreadItem {
 	
 	private Track _track = null;
-	private File _file = null;
+	private FileBody _fileBody = null;
+  private File _file = null;
 	
 	public Upload(File file){
-		this._file = file;
+    this._file = file;
 	}
 	
 
@@ -24,7 +31,9 @@ public class Upload extends ThreadItem {
 	protected synchronized void _run() {
 		
 		try {
-			AudioBoxClient.execute("", null, null, this._track, HttpPost.METHOD_NAME);
+
+
+			AudioBoxClient.execute("tracks", null, null, this._track, HttpPost.METHOD_NAME);
 		} catch (ServiceException e) {
 			e.printStackTrace( System.out );
 		} catch (LoginException e) {
@@ -47,7 +56,40 @@ public class Upload extends ThreadItem {
 
 	@Override
 	protected synchronized void start() {
-		this._track = new Track( this._file );
+
+    final Upload me = this;
+    final ThreadListener tl = this.getThreadListener();
+    final File file = this._file;
+    this._fileBody = new FileBody(this._file){
+      public void writeTo(final OutputStream out) throws IOException {
+
+        if (out == null) {
+            throw new IllegalArgumentException("Output stream may not be null");
+        }
+
+        long total = file.length(),
+          completed = 0;
+
+        InputStream in = new FileInputStream(file);
+        try {
+            byte[] tmp = new byte[4096];
+            int l;
+            while ((l = in.read(tmp)) != -1) {
+                out.write(tmp, 0, l);
+                completed += l;
+                tl.onProgress(me, total, completed, total-completed, file);
+            }
+            out.flush();
+        } finally {
+            in.close();
+        }
+      }
+
+    };
+
+    
+		this._track = new Track( this._fileBody);
+
 	}
 	
 	
