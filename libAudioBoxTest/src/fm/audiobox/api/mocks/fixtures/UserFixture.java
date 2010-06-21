05fcd4047ed550995483044a4b5a488853ec7d8f
Valerio Chiodino <keytwo@keytwo.net>
2010-06-21 22:25:29 +0200

    test

---
+++ libAudioBoxTest/src/fm/audiobox/api/mocks/fixtures/UserFixture.java
@@ -0,0 +1,10 @@
+package fm.audiobox.api.mocks.fixtures;
+
+public class UserFixture {
+    
+    public static final String LOGIN = "keytwo@keytwo.net";
+    public static final String RIGHT_PASS = "?m3rillion!";
+    public static final String USERNAME = "Keytwo";
+    public static final String WRONG_PASS = "asdasdasd";
+    
+}
