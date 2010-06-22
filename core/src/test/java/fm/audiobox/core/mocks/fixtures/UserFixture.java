c21e78ec13df692be7015ac77c207774b4d21d98
Valerio Chiodino <keytwo@keytwo.net>
2010-06-22 14:05:02 +0200

    gitignore

--- core/src/test/java/fm/audiobox/core/mocks/fixtures/UserFixture.java
+++ core/src/test/java/fm/audiobox/core/mocks/fixtures/UserFixture.java
@@ -5,6 +5,6 @@ public class UserFixture {
     public static final String LOGIN = "test@test.com";
     public static final String RIGHT_PASS = "test";
     public static final String USERNAME = "Test";
-    public static final String WRONG_PASS = "wrong pass";
+    public static final String WRONG_PASS = "wrong password";
     
 }
