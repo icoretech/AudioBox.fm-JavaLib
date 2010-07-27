eba1974d53036297b90a7fb4ab76e9768799561a
Fabio Tunno <fat@fatshotty.net>
2010-07-27 18:27:04 +0200

    implementation: onComplete

--- audiobox.fm-sync/src/main/java/fm/audiobox/sync/util/AsyncTaskManager.java
+++ audiobox.fm-sync/src/main/java/fm/audiobox/sync/util/AsyncTaskManager.java
@@ -2,6 +2,8 @@ package fm.audiobox.sync.util;
 
 import java.util.*;
 
+import fm.audiobox.sync.interfaces.ThreadListener;
+
 
 public class AsyncTaskManager {
 
@@ -11,9 +13,17 @@ public class AsyncTaskManager {
     private int max_threads = MAX;
     private boolean _autoStart = true;
     private boolean _stopped = false;
+    private boolean _completed = true;
+    
     protected ThreadGroup threadGroup = null;
+    private ThreadListener threadListener = new ThreadListener(){
+		public boolean onStart(AsyncTask result) {return true;}
+		public void onProgress(AsyncTask result, long total, long completed, long remaining, Object item) {}
+		public void onComplete(AsyncTask result, Object item) {}
+		public void onStop(AsyncTask task) {}
+    };
 
-    private List<AsyncTask> _threads = null;
+	private List<AsyncTask> _threads = null;
 
 
     public AsyncTaskManager(){
@@ -33,12 +43,22 @@ public class AsyncTaskManager {
         this.max_threads = maxThread;
         this._autoStart = autoStart;
     }
+    
+    
+    
+    public ThreadListener getThreadListener() {
+		return threadListener;
+	}
+
+	public void setThreadListener(ThreadListener threadListener) {
+		this.threadListener = threadListener;
+	}
+    
 
     public void newThread(AsyncTask item){
 
         if ( this._threads == null ) this._threads = new ArrayList<AsyncTask>();
         if ( this.threadGroup == null ) this.threadGroup = new ThreadGroup( this.generateThreadGroupName(item) );
-        
 
         item.setManager( this );
         this._threads.add( item );
@@ -59,12 +79,38 @@ public class AsyncTaskManager {
         }
     }
 
-    private void _start(){
+    private synchronized void _start(){
+    	/** Managements: 
+    	 * Stopped: return if thread manager was stopped
+    	 * No Threads found: return if no thread added
+    	 * Completed: fire 'onStart' event if thread manager was completed ( or at first call )
+    	 * Stack is full: return if Thread Stack is full
+    	 */
+    	
+    	/* Return if ThreadManager was stopped */
     	if ( this._stopped ) return;
+    	
+    	/* No Threads found */
     	if ( this._threads == null ) return;
+    	
+    	/* Thread Stack is full */
         if ( this.started_thread == this.max_threads ) return;
+        
+        /* Check if there are any thread */
         if ( (this.current_thread_index+1) >= this._threads.size() ) return;
-
+        
+        /* Fire 'onStart' event if ThreadManager was completed or is at first call */
+        if ( this._completed )
+        	this.getThreadListener().onStart(null);
+        
+        
+        
+        // Default: set false */
+        this._completed = false;
+        
+        /**
+         * Instantiate a new Thread and increment current index
+         */
         this.current_thread_index++;
 
         AsyncTask item = this._threads.get(this.current_thread_index);
@@ -72,6 +118,13 @@ public class AsyncTaskManager {
         thread.setName( this.generateThreadName(thread,item) );
 
         thread.start();
+        
+        /**
+         * Fire 'onProgress' event when thread is started
+         */
+        int _size = this._threads.size();
+        this.getThreadListener().onProgress(item, _size, this.current_thread_index ,_size - this.current_thread_index, null);
+        
         this.started_thread++;
 
     }
@@ -92,12 +145,20 @@ public class AsyncTaskManager {
 		this.current_thread_index = -1;
 	    this.started_thread = 0;
 	    
+	    this.getThreadListener().onStop(null);
+	    
 		this._stopped = false;
     }
     
 
-    public void onComplete(AsyncTask item) {
+    public synchronized void  onComplete(AsyncTask item) {
         this.started_thread--;
+        
+        if ( (this.current_thread_index+1) >= this._threads.size() ){
+        	this.getThreadListener().onComplete(null, null);
+        	return;
+        }
+        
         this._start();
     }
 
---
+++ audiobox.fm-sync/src/test/java/fm/audiobox/sync/test/ThreadTest.java
@@ -0,0 +1,109 @@
+package fm.audiobox.sync.test;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import fm.audiobox.sync.interfaces.ThreadListener;
+import fm.audiobox.sync.task.Scan;
+import fm.audiobox.sync.util.AsyncTask;
+import fm.audiobox.sync.util.AsyncTaskManager;
+
+public class ThreadTest extends junit.framework.TestCase {
+
+	private boolean finish = false;
+	
+	
+	@Test
+    @SuppressWarnings("deprecation")
+    public void testApp() {
+        
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
+        System.setProperty("org.apache.commons.logging.simplelog.log.httpclient.wire", "debug");
+        System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.commons.httpclient", "debug");
+        System.setProperty("org.apache.commons.logging.simplelog.log.httpclient.wire.header", "debug");
+        System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.impl", "debug");
+        System.setProperty("org.apache.commons.logging.simplelog.log.fm.audiobox.core", "debug");
+        System.setProperty("org.apache.commons.logging.simplelog.log.fm.audiobox.sync", "debug");
+        
+        assertTrue( true );
+        
+        AsyncTaskManager taskManager = new AsyncTaskManager(5,true);
+        
+        taskManager.setThreadListener(new ThreadListener() {
+			
+			@Override
+			public void onStop(AsyncTask task) {
+				System.out.println("Stop event");
+				
+			}
+			
+			@Override
+			public boolean onStart(AsyncTask result) {
+				System.out.println("Start");
+				return true;
+			}
+			
+			@Override
+			public void onProgress(AsyncTask result, long total, long completed,long remaining, Object item) {
+				System.out.println("Progress");
+			}
+			
+			@Override
+			public void onComplete(AsyncTask result, Object item) {
+				System.out.println("Complete");
+				//finish = true;
+			}
+		});
+        
+        List<File> files = Arrays.asList(new File("/home/fatshotty/Dropbox").listFiles(new FileFilter() {
+			public boolean accept(File pathname) {
+				return pathname.isDirectory();
+			}
+		})); 
+        System.out.println("Threads totali: " + files.size());
+        for( File file: files){
+        	Scan _scan = new Scan( file, true);
+            _scan.setFilter(new FileFilter() {
+    			@Override
+    			public boolean accept(File pathname) {
+    				return  true; 
+    			}
+    		});
+            _scan.setThreadListener(new ThreadListener() {
+				
+				@Override
+				public void onStop(AsyncTask task) {
+					System.out.println("Stop Scan");
+				}
+				
+				@Override
+				public boolean onStart(AsyncTask result) {
+					System.out.println("Start Scan");
+					return true;
+				}
+				
+				@Override
+				public void onProgress(AsyncTask result, long total, long completed,long remaining, Object item) {
+					System.out.println("Progress Scan");
+				}
+				
+				@Override
+				public void onComplete(AsyncTask result, Object item) {
+					System.out.println("\tComplete Scan");
+				}
+			});
+            taskManager.newThread(_scan);
+        }
+        
+        
+        while( !finish){}
+        
+        	
+    }
+	
+	
+}
