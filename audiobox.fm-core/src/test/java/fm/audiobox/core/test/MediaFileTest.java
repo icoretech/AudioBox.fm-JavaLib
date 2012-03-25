ab72c8f43c7a10e002558e7e0bbed741ca4fde7e
pinomasterflash <lucioregina@gmail.com>
2012-03-25 17:43:33 +0200

    new media file test

---
+++ audiobox.fm-core/src/test/java/fm/audiobox/core/test/MediaFileTest.java
@@ -0,0 +1,37 @@
+package fm.audiobox.core.test;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fm.audiobox.core.exceptions.LoginException;
+import fm.audiobox.core.exceptions.ServiceException;
+import fm.audiobox.core.models.MediaFile;
+import fm.audiobox.core.test.mocks.fixtures.Fixtures;
+
+public class MediaFileTest extends AudioBoxTestCase {
+
+  @Before
+  public void setUp() {
+    loginCatched();
+  }
+
+  @Test
+  public void testTrackActions() {
+    
+    try {
+      MediaFile media = new MediaFile(this.abc);
+      
+      File fileToUpload = new File( Fixtures.get("file_to_upload") ); 
+      media.upload( fileToUpload );
+      
+    } catch (ServiceException e) {
+      e.printStackTrace();
+    } catch (LoginException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+}
