0524f1c2b185672bb9b183780ccaa827342463b6
fatshotty <fat@fatshotty.net>
2012-03-25 16:34:54 +0200

    renamed file

--- audiobox.fm-core/src/test/java/fm/audiobox/core/test/MediaFileTest.java
+++ audiobox.fm-core/src/test/java/fm/audiobox/core/test/MediaFileTest.java
@@ -3,20 +3,21 @@ package fm.audiobox.core.test;
 import java.io.File;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import fm.audiobox.core.exceptions.LoginException;
 import fm.audiobox.core.exceptions.ServiceException;
 import fm.audiobox.core.models.MediaFile;
 import fm.audiobox.core.test.mocks.fixtures.Fixtures;
 
-public class MediaFileTests extends AudioBoxTestCase {
+public class MediaFileTest extends AudioBoxTestCase {
 
   @Before
   public void setUp() {
     loginCatched();
   }
 
-  //@Test
+  @Test
   public void testTrackActions() {
     
     
