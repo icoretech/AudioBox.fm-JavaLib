7433528d7b710a7228f74fb1c47f453718db9a97
fatshotty <fat@fatshotty.net>
2012-11-20 15:13:55 +0100

    remove warnings

--- audiobox.fm-core/src/test/java/fm/audiobox/core/test/mocks/models/Playlist.java
+++ audiobox.fm-core/src/test/java/fm/audiobox/core/test/mocks/models/Playlist.java
@@ -4,6 +4,8 @@ import fm.audiobox.interfaces.IConfiguration;
 
 public class Playlist extends fm.audiobox.core.models.Playlist {
 
+  private static final long serialVersionUID = 1L;
+
   public Playlist(IConfiguration config) {
     super(config);
   }
